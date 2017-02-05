ca87c486e88c9776f9e800356d5710b52f683d71
Samantha DiMaio <Samantha.DiMaio1@marist.edu>
2017-02-05 16:55:48 -0500

    Lab 2 - Problem 4 - 3.15 problem in textbook - Lottery 3 digit generator - Sam Dimaio: Feb 5th

--- labs/2/Lab2_prob4.java
+++ labs/2/Lab2_prob4.java
@@ -15,7 +15,7 @@ public class Lab2_prob4 {
     Scanner input = new Scanner(System.in);
     
     //generate a three digit number instead of two digit
-    int lottery = (int)(Math.random() * 1000)
+    int lottery = (int)(Math.random() * 1000);
     
     System.out.print("Enter your lottery guess (a three-digit number): ");
     int guess = input.nextInt();
@@ -25,23 +25,23 @@ public class Lab2_prob4 {
     int lotteryDigit2 = leftoverDigits / 10;
     int lotteryDigit3 = leftoverDigits % 10;
     
-    int guessDigit1 = lottery / 100;
-    int leftoverDigits = lottery % 100;
-    int guessDigit2 = leftoverDigits / 10;
-    int guessDigit3 = leftoverDigits % 10;
+    int guessDigit1 = guess / 100;
+    int leftoverDigits1 = guess % 100;
+    int guessDigit2 = leftoverDigits1 / 10;
+    int guessDigit3 = leftoverDigits1 % 10;
     
     System.out.println("The lottery number is: " + lottery);
     
     //Check guess
-    if (guess == loterry) {
+    if (guess == lottery) {
       System.out.println("Exact match: you win $10,000");
     }
 
     else if ((guessDigit1 == lotteryDigit2 && guessDigit2 == lotteryDigit3 && guessDigit3 == lotteryDigit1)
              || (guessDigit1 == lotteryDigit2 && guessDigit2 == lotteryDigit1 && guessDigit3 == lotteryDigit3) 
              || (guessDigit1 == lotteryDigit3 && guessDigit2 == lotteryDigit2 && guessDigit3 == lotteryDigit1)
-             || (guessDigit    
-    
+             || (guessDigit1 == lotteryDigit1 && guessDigit2 == lotteryDigit3 && guessDigit3 == lotteryDigit2)
+             || (guessDigit1 == lotteryDigit3 && guessDigit2 == lotteryDigit1 && guessDigit3 == lotteryDigit2))      
      {
       System.out.println("Match all digits: you win $3,000!");
     }
