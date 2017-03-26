4fac224f8996fa0c6632391186649a6865fe4187
Samantha DiMaio <Samantha.DiMaio1@marist.edu>
2017-03-26 17:18:10 -0400

    had to create for loops for input values, have to fix print out errors Project 1 Convolution Sam DiMaio

--- prj/1/Driver_prj1.java
+++ prj/1/Driver_prj1.java
@@ -14,27 +14,36 @@ public class Driver_prj1 {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
     
-    int[k] = input.nextInt();
-    int[i] = input.nextInt();
-    
+    int k = input.nextInt();
+    int i = input.nextInt();
+    int x;
+
     double[] vFirst = new double[k];    //declare vectors
     double[] vSecond = new double[i];
     
-    System.out.println(convolveVectors(vFirst[k], vSecond[i]));
+    for (x = 0; x < k; x++) {
+      vFirst[x] = input.nextDouble();  //input values
+    }
+    
+    for (x = 0; x < i; x++) {
+      vSecond[x] = input.nextDouble();
+    }
+    
+    double[] result = convolveVectors(vFirst, vSecond);
+    
+    System.out.println(result);
     
   }
-  
   public static double[] convolveVectors(double[] vFirst, double[] vSecond) {
     double[] vResult = new double[(vFirst.length + vSecond.length) - 1];
     for (int k = 0; k < vResult.length; k++) {
-       
       vResult[k] = 0;
       for (int i = 0; i < vSecond.length; i++) {
         if ((k - i >= 0) && (k - i < vFirst.length)) {
           vResult[k] += vFirst[(k - i)] * vSecond[i]; 
         }
-      }  
-      return vResult; 
-    }
+      }
+    }  
+    return vResult; 
   }
-}    
+}
