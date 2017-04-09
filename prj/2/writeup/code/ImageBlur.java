88c7f3b18aeafdde5737237b75ef24adf7ff8085
Samantha DiMaio <Samantha.DiMaio1@marist.edu>
2017-04-09 16:52:25 -0400

    working on setting up the window frame for the image and working in the constructor to read and load the original image onto the frame Sam DiMaio Image Expression April 9

--- prj/2/writeup/code/ImageBlur.java
+++ prj/2/writeup/code/ImageBlur.java
@@ -15,20 +15,53 @@ import java.util.Scanner;
 import java.awt.*
 import java.awt.image.*
 
-float[] elements = { .1111f, .1111f, .1111f, 
-                         .1111f, .1111f, .1111f,
-                         .1111f, .1111f, .1111f};
+//had to import packages that reads/loads image file
+import java.io.*;         //File()
+import javax.imageio.*;   //imageIO
+
 
 public class ImageBlur extends Object() //subclass of Java's Object 
 
+  private BufferedImage image; // class variable 
+
+  float[] elements = { .1111f, .1111f, .1111f, 
+                         .1111f, .1111f, .1111f,   //elements for convolution
+                         .1111f, .1111f, .1111f};
+                         
   public static void main(String[] args) { //main method
     //create object instance for setting up the window frame
     //ask user for dimensions of their image
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter the width of the image: ");
+    int w = input.nextInt();
+    System.out.println("Enter the height of the image: ");
+    int h = input.nextInt();
+    
+    Frame window = new Frame("ImageBlur");
+    f.setSize(new Dimension(w,h));
+    f.setVisible(true);  //method takes in boolean value to set Frame visible or not
+    
   }
   
   public ImageBlur() { //constructor 
-    //load the original image
-    //draw the original image 
+    Scanner input = new Scanner(System.in);
+    
+    setBackground(color.black) // insert optional background color? 
+    
+    BufferedImage pic = null;
+    
+    System.out.print("Enter the file name of your image: ") //ask user for file !name! of image 
+    String name = input.next()
+    
+    try {
+      pic = ImageIO.read(new File(name));
+    }                                         //reading the image file  	,      
+    catch (IOException e) {
+    }
+    
+    image = new BufferedImage(img.getWidth(this), img.getHeight(this), //type?)
+         
+    //draw the original image
   }
   
   public void create(Graphics g) { //method to design image
