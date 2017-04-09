0295e8a03b815f1afe151085c37a480b214f2df2
Samantha DiMaio <Samantha.DiMaio1@marist.edu>
2017-04-09 14:30:06 -0400

    Uploading Structure of the Image Expression project with comments of proposed ideas Sam DiMaio April 9

---
+++ prj/2/writeup/code/ImageBlur.java
@@ -0,0 +1,39 @@
+/**
+ * file: ImageBlur.java
+ * author: Samantha DiMaio
+ * course: CMPT 220
+ * assignment: Semester Project
+ * due date: May 10 2017
+ * version: 1.8
+ *
+ * This file contains the java source code to Image Expression 
+ * Image Expression blurs a desired image and asks the user to input a text onto the image
+ */
+
+//import packages
+import java.util.Scanner;
+import java.awt.*
+import java.awt.image.*
+
+float[] elements = { .1111f, .1111f, .1111f, 
+                         .1111f, .1111f, .1111f,
+                         .1111f, .1111f, .1111f};
+
+public class ImageBlur extends Object() //subclass of Java's Object 
+
+  public static void main(String[] args) { //main method
+    //create object instance for setting up the window frame
+    //ask user for dimensions of their image
+  }
+  
+  public ImageBlur() { //constructor 
+    //load the original image
+    //draw the original image 
+  }
+  
+  public void create(Graphics g) { //method to design image
+
+    //load the blurred image
+    //create adding text feature
+    //create desired blur amount feature
+  }
