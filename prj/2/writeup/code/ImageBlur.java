c61b86a27ef4dadc346da06e47b0b11649767901
Samantha DiMaio <Samantha.DiMaio1@marist.edu>
2017-04-23 15:44:52 -0400

    Semester Project major process , switched setBackground method to a frame attribute, got the kernel and convolveOp mehods to work, figured out how to draw image ---> it wasnt working because the method had to be named paint to use graphics2D April 23 SamDiMaio

--- prj/2/writeup/code/ImageBlur.java
+++ prj/2/writeup/code/ImageBlur.java
@@ -8,7 +8,7 @@
  *
  * This file contains the java source code to Image Expression 
  * Image Expression blurs a desired image and asks the user to input a text onto the image
- */
+*/
 
 //import packages
 import java.util.Scanner;
@@ -22,22 +22,26 @@ import java.applet.Applet;
 
 public class ImageBlur extends Applet { //subclass of Java's Object ----> Component
 
-  private BufferedImage image; // class variable 
+  private BufferedImage image;                               // class variables
   
-  private float elements = {.111, .111, .111, 
+  private float[] elements = {.1111f, .1111f, .1111f, 
+                              .1111f, .1111f, .1111f,
+                              .1111f, .1111f, .1111f};  
   
-  public static void main(String[] args) { //main method
+  public static void main(String[] args) {       //main method
     System.out.println("Welcome to Image Expression!");
+    System.out.println("----------------------------");
     
-    //create object instance for setting up the window frame
-    //ask user for dimensions of their image
     Scanner input = new Scanner(System.in);
-    System.out.print("Enter the width of the image: ");
-    int w = input.nextInt();                                
+    System.out.print("Enter the width of the image: ");   
+    int w = input.nextInt();                                 //ask user for dimensions of their image       
     System.out.print("Enter the height of the image: ");
     int h = input.nextInt();
     
+    
     Frame window = new Frame("ImageBlur");     //Calling Frame class from Applet 
+                                               //create object instance for setting up the window frame
+    window.setBackground(Color.cyan);          //calling color class to set color
     window.add("Center", new ImageBlur()); //
     window.setSize(new Dimension(w,h));   //
     window.setVisible(true);  //method takes in boolean value to set Frame visible or not
@@ -45,53 +49,51 @@ public class ImageBlur extends Applet { //subclass of Java's Object ----> Compon
   
   public ImageBlur() {   //constructor
     Scanner input = new Scanner(System.in);
-
-    setBackground(Color.pink); //set Background color to pink... make optional?
-    
     System.out.print("Enter the file name of your image: "); //ask user for file !name! of image 
     String name = input.next();   
-        
+    
+    BufferedImage pic = null;
+    
     try {
-      image = ImageIO.read(new File(name));                   
-    } 
-    catch (IOException e) {
+      pic = ImageIO.read(new File(name));                   
+    } catch (IOException e) {
     }
     
-    image = new BufferedImage(image.getWidth(this), image.getHeight(this), BufferedImage.TYPE_INT_RGB); //creating Buffered Image with width and height
+    image = new BufferedImage(pic.getWidth(this), pic.getHeight(this), BufferedImage.TYPE_INT_RGB); //creating Buffered Image with width and height
+    Graphics2D x = image.createGraphics();  //creates image & enables drawing on image 
     
-    Graphics2D x; //Graphics 2D variable
-    x = image.createGraphics();  //creates image & enables drawing on image 
-    
-    image.drawImage(image, 0, 0, this); //drawing original image 
+    x.drawImage(pic,0,0,this); //drawing original image (pic) with conditions of image 
   }
   
-  public void create(Graphics g) { //method to design image
+  public void paint(Graphics g) { //PAINT method must be used to design image using the Graphics class
+  
+    Graphics2D create = (Graphics2D) g; 
     
-
-    BufferedImage blur = new BufferedImage(image.getHeight(this), image.getHeight(this), BufferedImage.TYPE_INT_RGB); //make new buffered image for the blur
-                                         //use convolve op? & kernel methods to blur image
-                                         
-    Kernel k = new Kernal(0, 0, elements) //figure out width and height to use for kernel method
-    ConvolveOp convolve = new ConvolveOP(k) //convolve method takes in the kernel     
-    //find method to apply convolveOP
+    BufferedImage blur = new BufferedImage(image.getWidth(this), image.getWidth(this),BufferedImage.TYPE_INT_RGB); //make new buffered image for the blur
+           
+    //System.out.println("If you would like your image to have SUPER blur, enter 1");
+    //System.out.println("If not, enter 2");
     
-    System.out.println("If you would like your image to have SUPER blur, enter 1");
-    System.out.println("If not, enter 2");
+    //int superBlur = input.nextInt()
     
-    int superBlur = input.nextInt()
-    
-    if (superBlur == 1) {
-    }
+    //if (superBlur == 1) {
+    //}
       //to get super blur -- make a for loop that will change the pixels
     
-    else if (superBlur == 2) {
-    }
+    //else if (superBlur == 2) {
+    //}
+        
+	Kernel k = new Kernel(3, 3, elements); 
+    ConvolveOp c = new ConvolveOp(k);               //declaring method to blur based off Kernal
+    c.filter(image, blur);
+    
+    create.drawImage(blur, 0, 0, this);
+    
     //draw image again?
     //load the blurred image
     //create adding text feature
     //use Graphics2D & draw string method
     //create desired blur amount feature
   }
-  
-}
 
+}
